575571e3148acb76f5b56718aa1e12433d21f7a5
ARAFAT <munshiarafathussain@gmail.com>
2024-03-22 10:27:35 +0600

    Model created and test CRUD

--- index.js
+++ index.js
@@ -1,11 +1,104 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+const app = express();
+
+// middleware
+app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+
+// routes
+
+
 
 
 app.get('/', (req, res) => {
   res.send("Hello from Node API Backend Updated");
 });
+
+
+
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+
+
+app.get('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+
+
+app.put('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if(!product){
+      res.status(404).json({message: "Product Not Found"})
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+
+
+app.delete('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if(!product){
+      res.status(404).json({message: "Product Not Found"});
+    }
+    res.status(200).json({message: "Product Deleted Successfully"})
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+
+app.post('/api/products', async (req, res) => {
+  try{
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch(error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+
+
+
+
+
+mongoose.connect('mongodb+srv://arafathussain:h2kHYhmDrYYelG7x@cluster0.yjjshqp.mongodb.net/NodeCRUDapi?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('Connected to Database!');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch(() => console.log('Connection failed'));
+
+
+
+
+
